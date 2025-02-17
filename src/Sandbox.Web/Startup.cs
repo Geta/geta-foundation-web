f123a625a01de8849d4095fe6cf486d56487789b
Jevgenij Puchko <jevgen.pucko@gmail.com>
2025-02-17 13:59:18 +0200

    Init

---
+++ src/Sandbox.Web/Startup.cs
@@ -0,0 +1,17 @@
+
+namespace Sandbox.Web;
+
+public class Startup(IWebHostEnvironment webHostingEnvironment, IConfiguration configuration)
+{
+    private readonly Foundation.Startup _foundationStartup = new(webHostingEnvironment, configuration);
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        _foundationStartup.ConfigureServices(services);
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        _foundationStartup.Configure(app, env);
+    }
+}
